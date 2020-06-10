9aaf92f88efb39dc220df76eeb8d8f3c21bc2018
Roberto <io_robertob@hotmail.com>
2020-06-10 17:56:09 +0300

    update urls of files

--- audio_script.js
+++ audio_script.js
@@ -162,7 +162,7 @@ var filter = audioCtx.createBiquadFilter();
 
 // REVERB NODE 
 // 2) Load the impulse response; upon load, connect it to the audio output.
-var reverbUrl = "https://dev.ideas-block.com/files/AbernyteGrainSilo.m4a";
+var reverbUrl = "https://dev.ideas-block.com/mixer/files/AbernyteGrainSilo.m4a";
 var reverbNode = audioCtx.createReverbFromUrl(reverbUrl, function() {
   reverbNode.connect(audioCtx.destination);
 });
