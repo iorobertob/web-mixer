ef9b1000034e85f08d092eb207a79eeb9d76e069
Roberto <io_robertob@hotmail.com>
2020-07-13 14:15:04 +0300

    added text as 2 files

--- add_text.js
+++ add_text.js
@@ -7,10 +7,14 @@
           return false;
       }
 
-      httpRequest.onreadystatechange = setContents;
+      httpRequest.onreadystatechange      = setContents;
+      httpRequestIntro.onreadystatechange = setContentsIntro;
       //Open then send the request to your page
       httpRequest.open('GET', "https://dev.ideas-block.com/mixer/files/content_text.txt");
       httpRequest.send();
+
+      httpRequestIntro.open('GET', "https://dev.ideas-block.com/mixer/files/intro.txt");
+      httpRequestIntro.send();
        
 
       function setContents() {
@@ -18,6 +22,17 @@
          if (httpRequest.status === 200) {
           //Set the contents of your p tag
            document.getElementById("content_text").innerHTML = httpRequest.responseText;
+          } else {
+           alert('There was a problem with the request.');
+          }
+        }
+      }
+
+      function setContentsIntro() {
+       if (httpRequestIntro.readyState === XMLHttpRequest.DONE) {
+         if (httpRequestIntro.status === 200) {
+          //Set the contents of your p tag
+           document.getElementById("content_text_intro").innerHTML = httpRequestIntro.responseText;
           } else {
            alert('There was a problem with the request.');
           }
